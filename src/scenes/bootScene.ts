df21c0d9d27aa26040fdc02e7e1552e1b6a368d9
syntex01 <75858930+syntex01@users.noreply.github.com>
2025-10-12 17:55:58 +0200

    Scaffold autobattler scenes and shared state

---
+++ src/scenes/bootScene.ts
@@ -0,0 +1,44 @@
+export default class BootScene extends Phaser.Scene {
+  private orientationNotice?: Phaser.GameObjects.Text
+
+  constructor() {
+    super({
+      key: 'BootScene'
+    })
+  }
+
+  create() {
+    this.cameras.main.setBackgroundColor('#05060a')
+    const { centerX, centerY } = this.cameras.main
+
+    this.orientationNotice = this.add
+      .text(centerX, centerY, 'Rotate to landscape for the best experience', {
+        fontFamily: 'Arial',
+        fontSize: '24px',
+        color: '#ffffff',
+        align: 'center',
+        wordWrap: { width: this.cameras.main.width - 80 }
+      })
+      .setOrigin(0.5)
+      .setAlpha(0)
+
+    this.scale.on(Phaser.Scale.Events.ORIENTATION_CHANGE, this.handleOrientationChange, this)
+    this.events.once(Phaser.Scenes.Events.SHUTDOWN, this.onShutdown, this)
+    this.handleOrientationChange(this.scale.orientation)
+
+    this.time.delayedCall(150, () => {
+      this.scene.start('PreloadScene')
+    })
+  }
+
+  private handleOrientationChange(orientation: Phaser.Scale.Orientation) {
+    if (!this.orientationNotice) return
+
+    const isPortrait = orientation === Phaser.Scale.Orientation.PORTRAIT
+    this.orientationNotice.setAlpha(isPortrait ? 1 : 0)
+  }
+
+  private onShutdown() {
+    this.scale.off(Phaser.Scale.Events.ORIENTATION_CHANGE, this.handleOrientationChange, this)
+  }
+}
